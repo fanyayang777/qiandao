af508c0a120c2ae75bb409787e484b0b6320f09f
efanga <a13485478439@163.com>
2019-12-24 12:28:20 +0800

    config

---
+++ src/test/java/cn/qiandao/qiandao/QiandaoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.qiandao.qiandao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiandaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
